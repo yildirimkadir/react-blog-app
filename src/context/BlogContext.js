356120b4184957ccff897d41a2331e4ad6bed346
yildirimkadir <yildirimkad34@gmail.com>
2022-11-10 00:02:40 +0100

    "BlogContext wurde updated"

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -7,6 +7,24 @@ export const BlogContext = createContext();
 const BlogContextProvider = ({ children }) => {
     const [currentBlogs, setCurrentBlogs] = useState();
 
+    useEffect(() => {
+        const db = getDatabase(app);
+        const userRef = ref(db, "blogs/")
+        onValue(userRef, (snapshot) => {
+            const data = snapshot.val();
+            const blogL = [];
+            for (let id in blogs) {
+                blogL.push({ id, ...blogs[id] });
+            }
+            //   console.log(blogL);
+            setCurrentBlogs(blogL);
+            // setIsLoading(false)
+        })
+    }, [])
+
+
+
+
     return (
         <BlogContext.Provider value={{ currentBlogs }}>
             {children}
